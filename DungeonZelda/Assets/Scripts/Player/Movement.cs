238de18f1ee2c05a3ebc6e1d8937233c85626f66
RagsterTh <139650575+RagsterTh@users.noreply.github.com>
2025-03-13 13:49:09 -0300

    Implementando direção para interação e ataque

--- DungeonZelda/Assets/Scripts/Player/Movement.cs
+++ DungeonZelda/Assets/Scripts/Player/Movement.cs
@@ -10,22 +10,41 @@ public class Movement : MonoBehaviour
     Rigidbody2D rb;
     Bounds spriteRenderer;
     Vector2 inputDirections;
+    Vector3 lastPosition;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>().bounds;
+        lastPosition = direction.position;
     }
+    private void Update()
+    {
+        Vector3 face = direction.position - lastPosition;
+        if (face.magnitude > 0.01f)
+        {
 
+
+            float angle = Mathf.Atan2(face.y, face.x) * Mathf.Rad2Deg;
+
+            Quaternion targetRotation = Quaternion.Euler(0f, 0f, angle);
+
+            direction.rotation = targetRotation;
+
+
+            lastPosition = direction.position;
+        }
+    }
     // Update is called once per frame
     void FixedUpdate()
     {
         rb.velocity = inputDirections * speed;
+
     }
     public void Move(InputAction.CallbackContext value)
     {
         inputDirections = value.ReadValue<Vector2>();
-        //direction.position = new Vector2(bounds.min.x -0.5f, bounds.center.y);//Funcionou para a esquerda
+
 
     }
 }
